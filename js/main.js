40127a018b43f40aa372ce8c07aec682d2f88c78
carlo briganti <505641nl@saeinstitute.edu>
2020-08-31 16:33:35 +0200

    Festivita

--- js/main.js
+++ js/main.js
@@ -8,18 +8,51 @@ $(document).ready(function () {
   // start date
   var startDate = moment("2018-01-01");
   console.log(startDate);
-  var daysMonth = startDate.daysInMonth();
-  var month = startDate.format("MMMM");
-  console.log(daysMonth);
-  $("h1.calndar-month").html(month + " " + startDate.format("YYYY"));
-  for (var i = 1; i <= daysMonth; i++) {
-    var source = $("#days").html();
-    var template = Handlebars.compile(source);
-    var context = {
-      day: i,
-      month: month,
-    };
-    var html = template(context);
-    $(".container-calnedar").append(html);
-  }
+  createCal(startDate);
+  holiday(startDate)
 });
+function holiday (data){
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/holidays',
+        method: 'GET',
+        data: {
+            year:data.year(),
+            month:data.month()
+        },
+        success: function(risposta){
+            for(var i=0;i<risposta.response.length;i++){
+                var item = $('div[data-complete="'+risposta.response[i].date+ '"]');
+                item.append(risposta.response[i].name);
+                item.addClass('holiday');
+            }
+        },
+        error: function(){
+            alert('could not load calendar');
+        }
+    });
+}
+function createCal (data){
+    var daysMonth = data.daysInMonth();
+    var month = data.format("MMMM");
+    var monthNumber =  data.format("MM");
+    var year = data.format("YYYY");
+    $("h1.calndar-month").html(month + " " + data.format("YYYY"));
+    for (var i = 1; i <= daysMonth; i++) {
+      var source = $("#days").html();
+      var template = Handlebars.compile(source);
+      var context = {
+        day: addZero(i),
+        month: month,
+        dataCompleta: year +'-'+monthNumber+'-'+addZero(i)
+      };
+      var html = template(context);
+      $(".container-calnedar").append(html);
+    }
+};
+function addZero(n){
+    if (n < 10){
+        return 0 +''+n;
+    }else{
+        return n
+    }
+};
