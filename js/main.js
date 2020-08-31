489d52461dfc4efb6f2eae005c329ad0e22a752c
carlo briganti <505641nl@saeinstitute.edu>
2020-08-31 19:11:07 +0200

    tasti per andare aventi indietro

--- js/main.js
+++ js/main.js
@@ -7,52 +7,66 @@
 $(document).ready(function () {
   // start date
   var startDate = moment("2018-01-01");
-  console.log(startDate);
   createCal(startDate);
-  holiday(startDate)
+  holiday(startDate);
+  console.log(startDate.toString());
+  $("i.fa").click(function () {
+    if ($(this).hasClass("forward") == true) {
+      startDate = startDate.add(1, "M");
+      console.log(startDate.toString());
+     
+    } else {
+      startDate = startDate.subtract(1, "M");
+      console.log(startDate.toString());
+    }
+    $(".container-calnedar").empty();
+    $('.container-days').empty();
+    createCal(startDate);
+    holiday(startDate);
+  });
 });
-function holiday (data){
-    $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/holidays',
-        method: 'GET',
-        data: {
-            year:data.year(),
-            month:data.month()
-        },
-        success: function(risposta){
-            for(var i=0;i<risposta.response.length;i++){
-                var item = $('div[data-complete="'+risposta.response[i].date+ '"]');
-                item.append(risposta.response[i].name);
-                item.addClass('holiday');
-            }
-        },
-        error: function(){
-            alert('could not load calendar');
-        }
-    });
+function holiday(data) {
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/holidays",
+    method: "GET",
+    data: {
+      year: data.year(),
+      month: data.month(),
+    },
+    success: function (risposta) {
+      for (var i = 0; i < risposta.response.length; i++) {
+        var item = $('div[data-complete="' + risposta.response[i].date + '"]');
+        item.append(risposta.response[i].name);
+        item.addClass("holiday");
+      }
+    },
+    error: function () {
+      alert("could not load calendar");
+    },
+  });
 }
-function createCal (data){
-    var daysMonth = data.daysInMonth();
-    var month = data.format("MMMM");
-    var monthNumber =  data.format("MM");
-    var year = data.format("YYYY");
-    $("h1.calndar-month").html(month + " " + data.format("YYYY"));
-    for (var i = 1; i <= daysMonth; i++) {
-      var source = $("#days").html();
-      var template = Handlebars.compile(source);
-      var context = {
-        day: addZero(i),
-        month: month,
-        dataCompleta: year +'-'+monthNumber+'-'+addZero(i)
-      };
-      var html = template(context);
-      $(".container-calnedar").append(html);
-    }
-};
-function addZero(n){
-    if (n < 10){
-        return 0 +''+n;
-    }else{
-        return n
-    }
-};
+function createCal(data) {
+  var daysMonth = data.daysInMonth();
+  var month = data.format("MMMM");
+  var monthNumber = data.format("MM");
+  var year = data.format("YYYY");
+  $("h1.calndar-month").html(month + " " + data.format("YYYY"));
+  for (var i = 1; i <= daysMonth; i++) {
+    var source = $("#days").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      day: addZero(i),
+      month: month,
+      dataCompleta: year + "-" + monthNumber + "-" + addZero(i),
+    };
+    var html = template(context);
+    $(".container-calnedar").append(html);
+  }
+}
+function addZero(n) {
+  if (n < 10) {
+    return 0 + "" + n;
+  } else {
+    return n;
+  }
+}
