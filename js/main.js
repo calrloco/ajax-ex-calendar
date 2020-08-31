670fb87ce5633ed9cc7f37310c26581fd6575a25
carlo briganti <505641nl@saeinstitute.edu>
2020-08-31 15:30:43 +0200

    milestone 1 stampare giorni primo mese

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
 /** "response": [
- * {"name": "capodanno", 
+ * {"name": "capodanno",
  * "date": "2018-01-01"}
  * ] */
 
-$(document).ready(function(){
-  // start date 
-  var startDate = moment("2018-02-01");
+$(document).ready(function () {
+  // start date
+  var startDate = moment("2018-01-01");
   console.log(startDate);
-  var daysMonth = startDate.daysInMonth(); 
-  var month = startDate.format('MMMM');
+  var daysMonth = startDate.daysInMonth();
+  var month = startDate.format("MMMM");
   console.log(daysMonth);
-  $('h1.calndar-month').html(month+' '+ startDate.format('YYYY'));
-  for(var i=0;i<=daysMonth;i++){
-      var source = $('#days').html();
-      var template = Handlebars.compile(source);
-      var html = template(context);
-      var context = {
-          day: i,
-          month: month,
-      };
-      $('.container-calnedar').append(html);
+  $("h1.calndar-month").html(month + " " + startDate.format("YYYY"));
+  for (var i = 1; i <= daysMonth; i++) {
+    var source = $("#days").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      day: i,
+      month: month,
+    };
+    var html = template(context);
+    $(".container-calnedar").append(html);
   }
 });
